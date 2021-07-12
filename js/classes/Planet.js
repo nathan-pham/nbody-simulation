7603822d327b8d6931e91e25df7defbc71a508ad
Nathan Pham <nathanpham.me@gmail.com>
2021-07-12 14:21:28 -0700

    math & planets

--- js/classes/Planet.js
+++ js/classes/Planet.js
@@ -1,11 +1,42 @@
+import RandomGenerator from "./math/RandomGenerator"
+import Vector from "./math/Vector"
+import Utils from "./math/Utils"
+
+const generator = new RandomGenerator()
+const utils = new Utils()
+
 export default class Planet {
-    constructor(position, radius, mass) {
-        this.position = position
+    vel = new Vector(0, 0)
+    acc = new Vector(0, 0)
+
+    constructor({ radius, mass, pos }) {
         this.radius = radius
         this.mass = mass
+        this.pos = pos
     }
 
-    core(sketch) {
+    update(objects) {
+        for(const object of objects) {
+            const direction = new Vector(object.pos.x - this.pos.x, object.pos.y - this.pos.y)
+            const F = utils.force(object, this)
+            direction.setMag(F)
+
+            this.acc.add(direction)
+        }
         
+        this.vel.add(this.acc)
+        this.pos.add(this.vel)
+        this.acc.mult(0)
+    }
+
+    render(ctx) {
+        ctx.fillStyle = "#000"
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 2 * Math.PI);
+        ctx.fill()
+    }
+
+    core({ ctx, objects }) {
+        this.update(objects)
+        this.render(ctx)
     }
 }
---
+++ js/classes/math/Utils.js
@@ -0,0 +1,16 @@
+import Vector from "./Vector"
+
+const gravitationalConstant = 1
+
+export default class Utils {
+    distance(vec1, vec2) {
+        return Math.sqrt(Math.pow(vec2.x - vec1.x, 2) + Math.pow(vec2.y - vec1.y, 2))
+    }
+
+    force(object1, object2) {
+        const r = this.distance(object1.pos, object2.pos)
+        const F = gravitationalConstant * (object1.mass * object2.mass) / Math.pow(r, 2)
+        
+        return F
+    }
+}
